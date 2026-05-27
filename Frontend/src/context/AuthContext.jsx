ba075ae9b991bdc1e5c3ba874b8d0440f664bb7b
koliashwin <ashwinkoli02@gmail.com>
2026-05-27 11:43:18 +0530

    Frontend: Updated (fixed vercel page refresh issue & improved token based login)

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,14 +1,35 @@
 import React, { createContext, useContext, useEffect, useState } from "react"
+import {decodeToken} from '../utils/decodeToken';
 
 const AuthContext = createContext();
+
+const getStoredUser = () => {
+    try {
+        const stored = localStorage.getItem('user');
+        if (!stored) return null;
+        
+        const parsed = JSON.parse(stored);
+
+        // decode & check expiry
+        const decoded = decodeToken(parsed.access_token);
+        if (!decoded?.exp) return null;
+
+        const isExpired = decoded.exp * 1000 < Date.now();
+        if (isExpired) {
+            localStorage.removeItem('user');
+            return null
+        }
+        return parsed;
+    } catch (error) {
+        localStorage.removeItem('user');
+        return null;
+    }
+}
 
 export const AuthProvider = ({children}) => {
     
     // check the browser's storage if user exits then store it in state variable
-    const [user, setUser] = useState(() => {
-        const saved = localStorage.getItem('user');
-        return saved ? JSON.parse(saved) : null;
-    });
+    const [user, setUser] = useState(() => getStoredUser());
 
     // Create User and store it in browser's localStorage
     const login = (userData) => {
@@ -31,4 +52,6 @@ export const AuthProvider = ({children}) => {
     );
 };
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
